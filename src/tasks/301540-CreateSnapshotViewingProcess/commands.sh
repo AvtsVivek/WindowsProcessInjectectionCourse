805b89e0d27f459ff4de6f834bc833e0ec76bf4e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2024-10-24 17:24:46 +0530

    Added more info

---
+++ src/tasks/301540-CreateSnapshotViewingProcess/commands.sh
@@ -0,0 +1,27 @@
+
+cd ../../..
+
+cd src/tasks/301520-CreateToolHelpIntro
+
+cd src/apps/301520-CreateToolHelpIntro
+
+start .
+
+code .
+
+code . -r
+
+dir
+
+g++ --version
+
+# -o flag means, compile as well as link.
+g++ "-static" -o main.exe .\*.cpp
+
+# Try the following, if the above does not work.
+g++ "-static" -o main.exe .\*.cpp -std=c++20
+
+dir
+
+.\main.exe
+
