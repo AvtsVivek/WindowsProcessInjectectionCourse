eecb9b5ad3c8ee8bf63ec6994c0e2684e907aadc
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2025-09-20 19:08:30 +0530

    Added more info

---
+++ src/apps/405100-SimplePInvokeMessageBox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePInvokeMessageBox
+{
+    class Program
+    {
+        // Here we import our DLL that holds our `MessageBox` Function.
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string lpText, 
+            string lpCaption, uint uType);
+
+        static void Main(string[] args)
+        {
+            // Now we can call it using our required parameters.
+            MessageBox(IntPtr.Zero, "Hey there!", "Hello from P/Invoke!", 0);
+        }
+    }
+}
---
+++ src/tasks/405100-SimplePInvokeMessageBox/commands.sh
@@ -0,0 +1,21 @@
+
+cd ../../..
+
+cd src/tasks/405100-SimplePInvokeMessageBox
+
+cd src/apps/405100-SimplePInvokeMessageBox
+
+start .
+
+code .
+
+code . -r
+
+dir
+
+devenv /rebuild Debug SimplePInvokeMessageBox.sln
+
+dir
+
+dotnet run --project ./SimplePInvokeMessageBox.csproj
+
